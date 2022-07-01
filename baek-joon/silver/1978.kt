aea6f72b8a1cbc2fbf7f8664bb5533aacc8f12e5
minjong kim <alswhddkdl@gmail.com>
2022-07-01 17:35:21 +0900

    update baek-joon

---
+++ baek-joon/silver/1978.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)){
+        val n = nextInt()
+        var cnt = 0
+        repeat(n){
+                val a = nextInt()
+                if(isPrime(a)){
+                        cnt++
+                }
+        }
+        println(cnt)
+}
+fun isPrime(num:Int):Boolean{
+        if(num <= 1) return false
+        return (2..Math.sqrt(num.toDouble()).toInt()).none{ num % it == 0 }
+}
