63674ad48c4148bf1004a317bee5d65f2f1a3120
minjong kim <alswhddkdl@gmail.com>
2022-07-24 12:53:17 +0900

    update baekjoon

---
+++ baek-joon/silver/11650.kt
@@ -0,0 +1,20 @@
+import java.io.*
+
+
+fun main() = with(System.`in`.bufferedReader()){
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+        val n = readLine().toInt()
+        var pList = mutableListOf<Pair<Int,Int>>()
+        repeat(n){
+                var line = readLine().split(" ")
+                pList.add(Pair(line[0].toInt(),line[1].toInt()))
+        }
+        pList.sortWith(compareBy({it.first}, {it.second}))
+        for(i in pList){
+                bw.write("${i.first} ${i.second}\n")
+        }
+
+        bw.flush()
+        bw.close()
+        this.close()
+}
