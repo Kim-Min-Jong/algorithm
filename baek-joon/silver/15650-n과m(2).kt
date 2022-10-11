98672709c435a05721ae569b04ee442d7eafb4cd
minjong kim <alswhddkdl@gmail.com>
2022-10-11 13:11:27 +0900

    update baekjoon

---
+++ baek-joon/silver/15650-n과m(2).kt
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.*
+
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+fun main() = with(System.`in`.bufferedReader()){
+
+    val (n,m) = readLine().split(" ").map{it.toInt()}
+    val numList = List<Int>(n){i->i+1}
+    val answer = mutableListOf<List<Int>>()
+    combination(answer, numList, Array(numList.size){false},0,m)
+    answer.forEach { it ->
+        it.forEach {
+            bw.write("$it ")
+        }
+        bw.write("\n")
+    }
+    bw.flush()
+    bw.close()
+    close()
+}
+fun <T> combination(answer: MutableList<List<T>>, el: List<T>, ck: Array<Boolean>, start: Int, target: Int) {
+    if(target == 0) {
+        answer.addAll( listOf(el.filterIndexed { index, t -> ck[index] }) )
+    } else {
+        for(i in start until el.size) {
+            ck[i] = true
+            combination(answer, el, ck, i + 1, target - 1)
+            ck[i] = false
+        }
+    }
+}
