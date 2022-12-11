5e37ad83fb2db631dd57914e67d6efa4d8b6e11f
minjong kim <alswhddkdl@gmail.com>
2022-12-12 00:27:13 +0900

    update baekjoon

---
+++ baek-joon/silver/9656-돌 게임2.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.*
+
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+fun main() = with(System.`in`.bufferedReader()){
+
+    var n = readLine().toInt()
+    when{
+        n % 2 != 1 -> bw.write("SK")
+        else -> bw.write("CY")
+    }
+
+    bw.flush()
+    bw.close()
+    close()
+}
