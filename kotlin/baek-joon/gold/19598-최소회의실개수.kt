be8940d87eaa9466fa71763bd0a59803d573e005
minjong kim <alswhddkdl@gmail.com>
2023-01-25 09:27:20 +0900

    Update baekjoon

---
+++ kotlin/baek-joon/gold/19598-최소회의실개수.kt
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+import java.util.*
+
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val classList = mutableListOf<Time>()
+    val queue = PriorityQueue<Int>()
+    repeat(n) {
+        val (start, finish) = readLine().split(" ").map { it.toInt() }
+        classList.add(Time(start, finish))
+    }
+    classList.sortWith(compareBy<Time> { it.start })
+    queue.add(classList[0].end)
+    for(i in 1 until classList.size) {
+        if(classList[i].start < queue.peek())
+            queue.add(classList[i].end)
+        else {
+            queue.poll()
+            queue.add(classList[i].end)
+        }
+    }
+    bw.write("${queue.size}")
+    bw.flush()
+    bw.close()
+    close()
+}
+data class Time(
+    val start: Int,
+    val end:Int
+)
