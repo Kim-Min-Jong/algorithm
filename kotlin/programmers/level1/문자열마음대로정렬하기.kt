f2ffb680ef0861c50b4cb5c097ae9fb846a5ded3
minjong kim <alswhddkdl@gmail.com>
2023-02-05 01:35:43 +0900

    Update baekjoon

---
+++ kotlin/programmers/level1/문자열마음대로정렬하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(strings: Array<String>, n: Int): Array<String> {
+        var answer = arrayOf<String>()
+        answer = ((strings.sorted()).sortedBy{it[n]}).toTypedArray()
+        return answer
+    }
+}
