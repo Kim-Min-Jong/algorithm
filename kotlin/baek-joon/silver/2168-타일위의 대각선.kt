8381976cd3707473ebce262e251b22dd81ac3335
minjong kim <alswhddkdl@gmail.com>
2023-03-03 01:06:42 +0900

    update baekjoon

---
+++ kotlin/baek-joon/silver/2168-타일위의 대각선.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(" ").map{it.toInt()}
+    bw.write("${n + m - gcd(n, m)}")
+    
+    bw.flush()
+    bw.close()
+    close()
+}
+fun gcd(n: Int, m: Int): Int {
+    return if (n < m) {
+        if (n == 0) m else gcd(n, m % n)
+    } else {
+        if (m == 0) n else gcd(m, n % m)
+    }
+}
