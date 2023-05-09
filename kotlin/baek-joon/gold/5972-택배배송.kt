409ef3659573eadf883c115198f8845124713d12
minjong kim <alswhddkdl@gmail.com>
2023-05-09 10:42:46 +0900

    Update baekjoon

---
+++ kotlin/baek-joon/gold/5972-택배배송.kt
@@ -0,0 +1,52 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.*
+
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+const val INF = Int.MAX_VALUE
+lateinit var distance: IntArray
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (v, e) = readLine().split(" ").map{ it.toInt()}
+    val graph = Array(v) { mutableListOf<VariablePair>() }
+    distance = IntArray(v){ INF }
+    repeat(e) {
+        val (start, end, dist) = readLine().split(" ").map{it.toInt()}
+        graph[start-1].add(VariablePair(end-1, dist))
+        graph[end-1].add(VariablePair(start-1, dist))
+    }
+    dijkstra(0, graph)
+    bw.write("${distance.last()}")
+
+    bw.flush()
+    bw.close()
+    close()
+}
+data class VariablePair(
+    var first: Int,
+    var second: Int
+)
+fun dijkstra(startNum: Int, graph: Array<MutableList<VariablePair>>) {
+    // 현재위치, 가중치 가중치가 작은 순서로 정렬
+    val queue = PriorityQueue<Pair<Int, Int>> { o1, o2 ->
+        when {
+            o1.second < o2.second -> -1
+            o1.second > o2.second -> 1
+            else -> 0
+        }
+    }
+    queue.add(Pair(startNum, 0))
+    distance[startNum] = 0
+    while(queue.isNotEmpty()) {
+        val (now, dist) = queue.poll()
+        if(distance[now] < dist)
+            continue
+        for(next in graph[now]) {
+            val tmp = distance[now] + next.second
+            if(tmp < distance[next.first]) {
+                distance[next.first] = tmp
+                queue.add(Pair(next.first, tmp))
+            }
+        }
+    }
+}
