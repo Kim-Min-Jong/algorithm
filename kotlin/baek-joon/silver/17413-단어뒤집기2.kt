fa6b6f46dce2eaf4b85a168130d38dce29cad11c
minjong kim <alswhddkdl@gmail.com>
2023-05-16 17:25:55 +0900

    Update baekjoon(fixed)

--- kotlin/baek-joon/silver/17413-단어뒤집기2.kt
+++ kotlin/baek-joon/silver/17413-단어뒤집기2.kt
@@ -1,59 +1,9 @@
 import java.io.BufferedWriter
 import java.io.OutputStreamWriter
-import java.math.BigInteger
 import java.util.*
-import javax.lang.model.type.ArrayType
-import kotlin.math.abs
-import kotlin.math.log10
-import kotlin.math.min
-import kotlin.math.sqrt
-import kotlin.system.exitProcess
-
 
 val bw = BufferedWriter(OutputStreamWriter(System.out))
-lateinit var tmpList: MutableList<Int>
-lateinit var list: MutableList<Int>
-var a = 0
-var cnt = 0
-var flag = -1 // 0 -> b 1 -> w
-var overLabCount = 0
-lateinit var arr: List<Int>
-const val INF = 10000000
-lateinit var route: IntArray
-lateinit var graph_4963: Array<IntArray>
-lateinit var graph_2667: Array<IntArray>
-lateinit var graph_2468: Array<IntArray>
-lateinit var graph: Array<IntArray>
-lateinit var charGraph: Array<CharArray>
-lateinit var graph_10026: Array<CharArray>
-lateinit var visited: Array<BooleanArray>
-lateinit var distance: IntArray
-val dx = intArrayOf(1, -1, 0, 0)
-val dy = intArrayOf(0, 0, -1, 1)
-lateinit var resultSet: MutableSet<String>
-lateinit var tmp: MutableList<Char>
-var sheep = 0
-var wolf = 0
-val dp = Array<Long>(10000000) {0L}
-var pivot: Char = ' '
 fun main() = with(System.`in`.bufferedReader()) {
-//    //27087
-//    val n = readLine().toInt()
-//    repeat(n) {
-//        val (a, b, c, p) = readLine().split(" ").map{it.toInt()}
-//        var res = 0
-//
-//        for(i in listOf(a,b,c)) {
-//            if(i % p == 0) res++
-//        }
-//        val answer = if(res >= 1) 1 else 0
-//        bw.write("$answer\n")
-//
-//    }
-    // (100+1+ | 01)+
-//    val regex = "^(100+1+|01)+$".toRegex()
-//    val regex = "^[A-F]?A+F+C+[A-F]?$".toRegex()
-//    val regex = Regex(StringBuilder().append("^").append(prefix).append("[a-z]*").append(suffix).append("$").toString())
     val str = readLine()
     val stack = Stack<Char>()
     var chk = false
