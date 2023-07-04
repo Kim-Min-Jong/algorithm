c22205a922463766ba117f805084682a47b7c3ee
minjong kim <alswhddkdl@gmail.com>
2023-07-05 00:20:39 +0900

    Update baekjoon

---
+++ kotlin/baek-joon/gold/1188-음식평론가.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(" ").map{it.toInt()}
+    bw.write("${m - gcd(n,m)}")
+
+    bw.flush()
+    bw.close()
+    close()
+}
+fun gcd(n: Int, m: Int): Int {
+    return if (n < m) {
+        if (n == 0) m else gcd(n, m % n)
+    } else {
+        if (m == 0) n else gcd(m, n % m)
+    }
+}
