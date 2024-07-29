2feae11125884c74520757fba15f3f851c30bb2e
minjong kim <alswhddkdl@gmail.com>
2024-07-29 16:45:12 +0900

    Update programmers(sql)

---
+++ kotlin/programmers/sql/lv2-3월에 태어난 여성 회원목록.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT member_id, member_name, gender, to_char(date_of_birth,'YYYY-MM-DD') from member_profile where tlno is not null and to_char(date_of_birth,'MM') = '03' and gender = 'W' order by member_id
