09d697df5f2bb7e4d91fcd188e5d05d3532fdfd0
minjong kim <alswhddkdl@gmail.com>
2025-02-12 11:59:42 +0900

    Update sql

---
+++ kotlin/programmers/sql/lv4-취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT a.apnt_no, p.pt_name, a.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd from patient p, doctor d, appointment a where a.pt_no = p.pt_no and a.mddr_id = d.dr_id and a.apnt_cncl_yn = 'N' and a.mcdp_cd = 'CS' and to_char(a.apnt_ymd,'YYYYMMDD') = '20220413' order by a.apnt_ymd asc
