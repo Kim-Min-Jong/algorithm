7ecde98fc428def63642dc53216f41517355be5f
minjong kim <alswhddkdl@gmail.com>
2025-02-13 21:32:16 +0900

    Update sql

---
+++ kotlin/programmers/sql/lv3-카테고리별 도서 판매량 집계하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT b.category, sum(s.sales) from book b, book_sales s where b.book_id = s.book_id and to_char(s.sales_date,'YYYY-MM') = '2022-01' group by b.category order by b.category
